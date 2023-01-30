38c764d269e2c3330a34dce9db862be5b2d75ac8
CH_Hwang <hch950627@naver.com>
2023-01-30 20:55:37 +0900

    feat: any

--- 완주하지못한선수/index.js
+++ 완주하지못한선수/index.js
@@ -8,9 +8,14 @@ function solution(participant, completion) {
     const part = participant[i];
     const comp = completion[i];
 
+    // console.log(part, comp);
+
     obj[part] = obj[part] ? obj[part] + 1 : 1; // 참가자명단에 있으면 +1
+    // console.log('참가자 확인', obj);
     obj[comp] = obj[comp] ? obj[comp] - 1 : -1; // 완료명단에 있으면 -1
+    // console.log('완주자 확인', obj);
   }
+  console.log(obj);
 
   for (const key in obj) {
     if (obj[key] > 0) return key; // 참가자 명단에 있지만 완료명단에 없는사람은 +_
